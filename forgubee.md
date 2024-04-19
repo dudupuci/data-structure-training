74c470b48f36df907f6c58359820991dcbd5b70d
Eduardo Pucinelli <dudupucinelli@gmail.com>
2024-04-19 02:44:24 -0300

    :sparkles: exercise-3

--- forgubee.md
+++ forgubee.md
@@ -4,3 +4,6 @@
 
 - Segundo Exercicio:
 ![img_2.png](img_2.png)
+
+- Terceiro Exercicio:
+![img_3.png](img_3.png)
--- src/main/java/io/github/com/dudupuci/structure/data/Main.java
+++ src/main/java/io/github/com/dudupuci/structure/data/Main.java
@@ -1,60 +1,24 @@
 package io.github.com.dudupuci.structure.data;
 
-import io.github.com.dudupuci.structure.data.entities.Child;
-import io.github.com.dudupuci.structure.data.utils.NameGenerator;
-
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        LinkedList<Child> children = new LinkedList<>();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Quantas criancas vão brincar?");
-        int childrenSize = scanner.nextInt();
-
-        for (int i = 0; i <= childrenSize; i++) {
-            children.add(new Child(NameGenerator.generate()));
+    public static void main(String[] args) {
+        var numbers = showNumbers();
+        for (Integer number : numbers) {
+            System.out.println(number);
         }
+    }
 
-
-        var chosenChild = chooseChildToSpeak(children);
-        System.out.println("A crianca que vai gritar é a " + chosenChild.getName() + ", ele não vai brincar, só gritar!");
-        children.remove(chosenChild);
-
-        var playing = children.stream().map(Child::getName).toList();
-        System.out.println("Quem está jogando é " + playing);
-
-        Iterator<Child> iterator = children.iterator();
-
-        while (children.size() > 1) {
-            while (iterator.hasNext()) {
-                Child currentChild = iterator.next();
-                System.out.println(chosenChild.getName() + " diz " + chosenChild.talkHotPotato());
-                Thread.sleep(500);
-
-                if (isHotPotatoBurned()) {
-                    System.out.println(chosenChild.burn(currentChild));
-                    iterator.remove();
-                }
+    private static List<Integer> showNumbers() {
+        var numbers = new ArrayList<Integer>();
+        for (int i = 1000; i <= 1999; i++) {
+            if (i % 11 == 5) {
+                numbers.add(i);
             }
-            iterator = children.iterator();
         }
-
-        System.out.println("---------------------");
-        System.out.println("Winner: " + children.getFirst());
-        System.out.println("---------------------");
-    }
-
-    private static Child chooseChildToSpeak(LinkedList<Child> children) {
-        var random = new Random();
-        var index = random.nextInt(children.size());
-        return children.get(index);
-    }
-
-    private static boolean isHotPotatoBurned() {
-        return Math.random() < 0.5;
+        return numbers;
     }
 }
